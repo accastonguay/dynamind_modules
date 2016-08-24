3ad14ee6cde88f9b74ffc36ce412b4fd07d9ab67
accastonguay <kyrielle53@yahoo.ca>
2016-08-24 12:35:44 +1000

    new present_value module, averaged water price

---
+++ new_rwht/new_rwht/new_rwht_module.py
@@ -0,0 +1,223 @@
+__author__ = 'acharett'
+
+
+from pydynamind import *
+from osgeo import ogr
+from random import *
+
+class NewRwhtModule(Module):
+        display_name = "rwht choice"
+        group_name = "ABM"
+        """
+        Module Initialisation
+        """
+        def __init__(self):
+            Module.__init__(self)
+
+            #To use the GDAL API
+            self.setIsGDALModule(True)
+            #Parameter Definition
+
+            self.createParameter("quartile", INT)
+            self.quartile = 2
+
+
+
+            self.rwht_option = ViewContainer("rwht_option", COMPONENT, READ)
+            # self.rwht_option.addAttribute("plumbed", Attribute.DOUBLE, READ)
+            self.rwht_option.addAttribute("year", Attribute.INT, READ)
+            self.rwht_option.addAttribute("volume", Attribute.DOUBLE, READ)
+            self.rwht_option.addAttribute("annual_water_savings", Attribute.DOUBLE, READ)
+            self.rwht_option.addAttribute("non_potable_savings", Attribute.DOUBLE, READ)
+            self.rwht_option.addAttribute("price", Attribute.DOUBLE, READ)
+            # self.rwht_option.addAttribute("annual_water_savings", Attribute.DOUBLE, READ)
+            self.rwht_option.addAttribute("outdoor_water_savings", Attribute.DOUBLE, READ)
+
+            # self.rwht_option.addAttribute("cost_factor", Attribute.DOUBLE, READ)
+            # self.rwht_option.addAttribute("indoor_factor", Attribute.DOUBLE, READ)
+            # self.rwht_option.addAttribute("outdoor_factor", Attribute.DOUBLE, READ)
+
+            self.rwht_option.addAttribute("pv_total_costs_indoor", Attribute.DOUBLE, WRITE)
+            self.rwht_option.addAttribute("pv_total_costs_outdoor", Attribute.DOUBLE, WRITE)
+
+            self.rwht_option.addAttribute("pv_non_potable_saving", Attribute.DOUBLE, WRITE)
+            self.rwht_option.addAttribute("pv_outdoor_saving", Attribute.DOUBLE, WRITE)
+
+            self.rwht_option.addAttribute("pv_indoor", Attribute.DOUBLE, WRITE)
+            self.rwht_option.addAttribute("pv_outdoor", Attribute.DOUBLE, WRITE)
+
+
+
+        # self.registerViewContainers([self.__rwht])
+
+            #Compile views
+            views = []
+            views.append(self.rwht_option)
+
+            #Register ViewContainer to stream
+            self.registerViewContainers(views)
+
+            #Data Stream Definition
+
+
+        """
+        Data Manipulation Process (DMP)
+        """
+        construction_cost_indoor_db = {
+            2000: {2: 1854, 10: 2299, 5: 2262},
+            2001: {2: 1936, 10: 2401, 5: 2362},
+            2002: {2: 1995, 10: 2474, 5: 2434},
+            2003: {2: 2051, 10: 2544, 5: 2503},
+            2004: {2: 2092, 10: 2595, 5: 2553},
+            2005: {2: 2144, 10: 2659, 5: 2616},
+            2006: {2: 2213, 10: 2745, 5: 2700},
+            2007: {2: 2264, 10: 2809, 5: 2763},
+            2008: {2: 2359, 10: 2927, 5: 2879},
+            2009: {2: 2393, 10: 2968, 5: 2919},
+            2010: {2: 2467, 10: 3060, 5: 3010},
+            2011: {2: 2552, 10: 3165, 5: 3114},
+            2012: {2: 2593, 10: 3216, 5: 3164},
+            2013: {2: 2657, 10: 3296, 5: 3242},
+            2014: {2: 2719, 10: 3372, 5: 3317},
+            2015: {2: 2757, 10: 3420, 5: 3364}}
+        construction_cost_outdoor_db = {
+            2000: {2: 1395, 10: 1840, 5: 1803},
+            2001: {2: 1456, 10: 1922, 5: 1882},
+            2002: {2: 1501, 10: 1980, 5: 1940},
+            2003: {2: 1543, 10: 2036, 5: 1995},
+            2004: {2: 1574, 10: 2077, 5: 2035},
+            2005: {2: 1613, 10: 2128, 5: 2085},
+            2006: {2: 1665, 10: 2197, 5: 2152},
+            2007: {2: 1704, 10: 2248, 5: 2202},
+            2008: {2: 1775, 10: 2342, 5: 2294},
+            2009: {2: 1800, 10: 2375, 5: 2327},
+            2010: {2: 1856, 10: 2449, 5: 2399},
+            2011: {2: 1920, 10: 2534, 5: 2482},
+            2012: {2: 1951, 10: 2574, 5: 2522},
+            2013: {2: 1999, 10: 2638, 5: 2584},
+            2014: {2: 2046, 10: 2699, 5: 2644},
+            2015: {2: 2075, 10: 2737, 5: 2681}}
+
+        discount_rate = 0.05
+        years = []
+
+        discount_factor = []
+        for yr in xrange(1, 21, 1):
+            years.append(yr)
+            discount_factor.append(1. / (1. + discount_rate) ** (yr - 1))
+
+        def construction_costs_indoor(year, size):
+            """
+            Returns construction costs based on the tank size
+            :param size:
+            :return:
+            """
+
+            try:
+                return construction_cost_indoor_db[year][int(size)]
+            except:
+                raise LookupError("No such tank found")
+
+        def construction_costs_outdoor(year, size):
+            """
+            Returns construction costs based on the tank size
+            :param size:
+            :return:
+            """
+            try:
+                return construction_cost_outdoor_db[year][int(size)]
+            except:
+                raise LookupError("No such tank found")
+
+        def maintenance_costs():
+            maintenance_cost = []
+            for yrs in years:
+                maintenance_cost.append(20.)
+
+            discount_maintenance_cost = [d * m for d, m in zip(discount_factor, maintenance_cost)]
+
+            return sum(discount_maintenance_cost)
+
+        def pv_total_costs_indoor_fun(year, volume):
+
+            try:
+                return construction_costs_indoor(year, volume) + maintenance_costs()
+            except (LookupError):
+                raise ValueError("can't calculate total costs")
+
+        def pv_total_costs_outdoor_fun(year, volume):
+            try:
+                return construction_costs_outdoor(year, volume) + maintenance_costs()
+            except (LookupError):
+                raise ValueError("can't calculate total costs")
+
+        def pv_non_potable_saving_fun(annual_water_savings, price):
+            indoor_water_price = []
+            # points = np.array([(2005, 1.17), (2008,  1.62), (2009,  1.85), (2010, 2.1), (2011, 2.36), (2012, 2.11), (2013, 2.59), (2014, 2.55), (2015, 2.62)])
+            # # get x and y vectors
+            # x = points[:,0]
+            # y = points[:,1]
+            # # calculate polynomial
+            # z = np.polyfit(x, y,3)
+            # f = np.poly1d(z)
+
+            for y in years:
+                # indoor_water_price.append(2.5)
+                indoor_water_price.append(price)
+
+            discount_non_potable_savings = [d * m * annual_water_savings for d, m in
+                                            zip(discount_factor, indoor_water_price)]
+
+            return sum(discount_non_potable_savings)
+
+        def pv_outdoor_saving_fun(outdoor_water_savings, price):
+            indoor_water_price = []
+
+            for y in years:
+                # indoor_water_price.append(2.5)
+                indoor_water_price.append(price)
+
+            discount_outdoor_savings = [d * m * outdoor_water_savings for d, m in
+                                        zip(discount_factor, indoor_water_price)]
+
+            return sum(discount_outdoor_savings)
+
+        def run(self):
+            #Data Stream Manipulation
+
+
+            self.rwht_option.reset_reading()
+
+            for r in self.rwht_option:
+
+                y = r.GetFieldAsInteger("year")
+                v = r.GetFieldAsDouble("volume")
+                p = r.GetFieldAsDouble("price")
+                # f = r.GetFieldAsDouble("cost_factor")
+
+                # out_factor = r.GetFieldAsDouble("outdoor_factor")
+                # in_factor = r.GetFieldAsDouble("indoor_factor")
+
+                # Calculate PV for indoor use
+                # print 'year is: ',y
+                pv_total_costs_indoor = pv_total_costs_indoor_fun(y, v)
+                # pv_non_potable_saving = pv_non_potable_saving_fun((r.GetFieldAsDouble("outdoor_water_savings")*out_factor+r.GetFieldAsDouble("non_potable_savings")*in_factor), p)
+                pv_non_potable_saving = pv_non_potable_saving_fun((r.GetFieldAsDouble("outdoor_water_savings")+r.GetFieldAsDouble("non_potable_savings")), p)
+                pv_indoor = pv_non_potable_saving - pv_total_costs_indoor
+                r.SetField("pv_total_costs_indoor", pv_total_costs_indoor)
+                r.SetField("pv_non_potable_saving", pv_non_potable_saving)
+                r.SetField("pv_indoor", pv_indoor)
+
+                # Calculate PV for outdoor use
+                pv_total_costs_outdoor = pv_total_costs_outdoor_fun(y, v)
+                # pv_outdoor_saving = pv_outdoor_saving_fun((r.GetFieldAsDouble("outdoor_water_savings")*out_factor), p)
+                pv_outdoor_saving = pv_outdoor_saving_fun(r.GetFieldAsDouble("outdoor_water_savings"), p)
+                pv_outdoor = pv_outdoor_saving - pv_total_costs_outdoor
+                r.SetField("pv_total_costs_outdoor", pv_total_costs_outdoor)
+                r.SetField("pv_outdoor_saving", pv_outdoor_saving)
+                r.SetField("pv_outdoor", pv_outdoor)
+
+
+
+
+            self.rwht_option.finalise()
--- water_price_parcel/water_price_parcel/water_price_parcel/water_price_parcel.py
+++ water_price_parcel/water_price_parcel/water_price_parcel/water_price_parcel.py
@@ -20,25 +20,43 @@ class WaterPriceParcel(Module):
             self.setIsGDALModule(True)
             #Parameter Definition
 
-            self.createParameter("price", DOUBLE)
-            self.price = 1
+            self.createParameter("time", INT)
+            self.time = 1
 
 
+            points = np.array([(2003, 0.7757),(2004, 0.88), (2005,  0.92),(2006, 0.96), (2007,  0.99)])
+
+            x = points[:,0]
+            y = points[:,1]
+            # calculate polynomial
+            z = np.polyfit(x, y,1)
+            self.__f = np.poly1d(z)
+
+            self.__dict1 = {2004: 0.75, 2005: 0.78, 2006: 0.81, 2007: 0.85, 2008: 1.01, 2009: 1.25, 2010: 1.53, 2011: 1.77, 2012: 1.77, 2013: 2.59, 2014: 2.55, 2015: 2.62}
+            self.__dict2 = {2004: 0.88, 2005: 0.92, 2006: 0.96, 2007: 0.99, 2008: 1.19, 2009: 1.47, 2010: 1.8, 2011: 2.08, 2012: 2.08, 2013: 3.04, 2014: 2.99, 2015: 3.07}
+            self.__dict3 = {2004: 1.3, 2005: 1.35, 2006: 1.41, 2007: 1.47, 2008: 1.76, 2009: 2.17, 2010: 2.65, 2011: 3.07, 2012: 3.07, 2013: 4.50, 2014: 4.42, 2015: 4.54}
+
+            self.__dict4 = {2005: 0.75, 2006: 0.7842, 2007: 0.8184, 2008: 0.8517, 2009: 1.0192, 2010: 1.2532, 2011: 1.5343, 2012: 1.7756, 2013: 1.7756, 2014: 2.597, 2015: 2.5523}
+            self.__dict5 = {2005: 0.88, 2006: 0.92005, 2007: 0.9601, 2008: 0.9992, 2009: 1.1957, 2010: 1.4702, 2011: 1.8, 2012: 2.0832, 2013: 2.0832, 2014: 3.0469, 2015: 2.9944}
+            self.__dict6 = {2005: 1.3, 2006: 1.35925, 2007: 1.4185, 2008: 1.4763, 2009: 1.7666, 2010: 2.1721, 2011: 2.6594, 2012: 3.0778, 2013: 3.0778, 2014: 4.5017, 2015: 4.4242}
+
+
+            self.__dict7 = {2004: 0.76285, 2005: 0.7671, 2006: 0.8013, 2007: 0.83505, 2008: 0.93545, 2009: 1.1362, 2010: 1.39375, 2011: 1.65495, 2012: 1.7756, 2013: 2.1863, 2014: 2.57465, 2015: 2.58615}
+            self.__dict8 = {2004: 0.82785, 2005: 0.900025, 2006: 0.940075, 2007: 0.97965, 2008: 1.09745, 2009: 1.33295, 2010: 1.6351, 2011: 1.9416, 2012: 2.0832, 2013: 2.56505, 2014: 3.02065, 2015: 3.0322}
+            self.__dict9 = {2004: 1.03785, 2005: 1.329625, 2006: 1.388875, 2007: 1.4474, 2008: 1.62145, 2009: 1.96935, 2010: 2.41575, 2011: 2.8686, 2012: 3.0778, 2013: 3.78975, 2014: 4.46295, 2015: 4.4821}
+
+        def init(self):
             self.parcel = ViewContainer("parcel", COMPONENT, READ)
 
             self.parcel.addAttribute("year", Attribute.INT, READ)
             self.parcel.addAttribute("total_daily_demand", Attribute.DOUBLE, READ)
             self.parcel.addAttribute("price", Attribute.DOUBLE, WRITE)
-            # self.city.addAttribute("inflow", Attribute.DOUBLE, WRITE)
-
-
+            self.parcel.addAttribute("time", Attribute.INT, WRITE)
+            self.parcel.addAttribute("level", Attribute.INT, WRITE)
 
             #Compile views
             views = []
-            # views.append(self.city_council)
             views.append(self.parcel)
-            # views.append(self.city)
-
 
             #Register ViewContainer to stream
             self.registerViewContainers(views)
@@ -53,76 +71,80 @@ class WaterPriceParcel(Module):
         def run(self):
             #Data Stream Manipulation
 
-            #print self.my_parameter
-
-            #Data Stream Manipulation
-
-            #print self.my_parameter
             self.parcel.reset_reading()
             # self.city.reset_reading()
             '''Price dictionaries for three categories of uses if year = y+1 for y-y+1 period '''
 
-            points = np.array([(2004, 0.7757),(2005, 0.88), (2006,  0.92),(2007, 0.96), (2008,  0.99)])
-            # get x and y vectors
-            x = points[:,0]
-            y = points[:,1]
-            # calculate polynomial
-            z = np.polyfit(x, y,1)
-            f = np.poly1d(z)
-            dict1 = {2005: 0.75, 2006: 0.7842, 2007: 0.8184, 2008: 0.8517, 2009: 1.0192, 2010: 1.2532, 2011: 1.5343, 2012: 1.7756, 2013: 1.7756, 2014: 2.597, 2015: 2.5523}
-            dict2 = {2005: 0.88, 2006: 0.92005, 2007: 0.9601, 2008: 0.9992, 2009: 1.1957, 2010: 1.4702, 2011: 1.8, 2012: 2.0832, 2013: 2.0832, 2014: 3.0469, 2015: 2.9944}
-            dict3 = {2005: 1.3, 2006: 1.35925, 2007: 1.4185, 2008: 1.4763, 2009: 1.7666, 2010: 2.1721, 2011: 2.6594, 2012: 3.0778, 2013: 3.0778, 2014: 4.5017, 2015: 4.4242}
-
-
             for b in self.parcel:
                 year = b.GetFieldAsInteger("year")
                 demand = b.GetFieldAsDouble("total_daily_demand")
-                if year < 2004:
-                        p = f(year)
+                if self.time == 1:
+
+                    if year < 2003:
+                        p = self.__f(year)
                         b.SetField("price", p)
-                elif year == 2004:
+                        b.SetField("level", 1)
+                    elif year == 2003:
                         b.SetField("price", 0.7757)
-                elif year >= 2005:
-                    if demand <440:
-                        p = dict1[year]
+                        b.SetField("level", 1)
+                    elif year >= 2004:
+                        if demand < 440:
+                            p = self.__dict1[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 1)
+                        elif 440 <= demand < 880:
+                            p = self.__dict2[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 2)
+                        elif demand >= 880:
+                            p = self.__dict3[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 3)
+                    b.SetField("time", 1)
+
+                elif self.time == 2:
+                    if year < 2004:
+                        p = self.__f(year)
                         b.SetField("price", p)
-                    elif 440 <= demand < 880:
-                        p = dict2[year]
+                        b.SetField("level", 1)
+                    elif year == 2004:
+                            b.SetField("price", 0.7757)
+                            b.SetField("level", 1)
+                    elif year >= 2005:
+                        if demand <440:
+                            p = self.__dict4[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 1)
+                        elif 440 <= demand < 880:
+                            p = self.__dict5[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 2)
+                        elif demand >= 880:
+                            p = self.__dict6[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 3)
+                    b.SetField("time", 2)
+
+                elif self.time == 3:
+                    if year < 2004:
+                        p = self.__f(year)
                         b.SetField("price", p)
-                    elif demand >= 880:
-                        p = dict3[year]
-                        b.SetField("price", p)
+                        b.SetField("level", 1)
+                    elif year == 2004:
+                            b.SetField("price", 0.7757)
+                            b.SetField("level", 1)
+                    elif year >= 2005:
+                        if demand <440:
+                            p = self.__dict7[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 1)
+                        elif 440 <= demand < 880:
+                            p = self.__dict8[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 2)
+                        elif demand >= 880:
+                            p = self.__dict9[year]
+                            b.SetField("price", p)
+                            b.SetField("level", 3)
+                    b.SetField("time", 2)
             self.parcel.finalise()
-
-            '''Price dictionaries for three categories of uses if year = y for y-y+1 period '''
-
-            # points = np.array([(2003, 0.7757),(2004, 0.88), (2005,  0.92),(2006, 0.96), (2007,  0.99)])
-            # # get x and y vectors
-            # x = points[:,0]
-            # y = points[:,1]
-            # # calculate polynomial
-            # z = np.polyfit(x, y,1)
-            # f = np.poly1d(z)
-            #
-            # dict1 = {2004: 0.75, 2005: 0.7842, 2006: 0.8184, 2007: 0.8517, 2008: 1.0192, 2009: 1.2532, 2010: 1.5343, 2011: 1.7756, 2012: 1.7756, 2013: 2.597, 2014: 2.5523, 2015: 2.62}
-            # dict2 = {2004: 0.88, 2005: 0.92005, 2006: 0.9601, 2007: 0.9992, 2008: 1.1957, 2009: 1.4702, 2010: 1.8, 2011: 2.0832, 2012: 2.0832, 2013: 3.0469, 2014: 2.9944, 2015: 3.07}
-            # dict3 = {2004: 1.3, 2005: 1.35925, 2006: 1.4185, 2007: 1.4763, 2008: 1.7666, 2009: 2.1721, 2010: 2.6594, 2011: 3.0778, 2012: 3.0778, 2013: 4.5017, 2014: 4.4242, 2015: 4.54}
-            # for b in self.parcel:
-            #     year = b.GetFieldAsInteger("year")
-            #     demand = b.GetFieldAsDouble("total_daily_demand")
-            #     if year < 2003:
-            #             p = f(year)
-            #             b.SetField("price", p)
-            #     elif year == 2003:
-            #             b.SetField("price", 0.7757)
-            #     elif year >= 2004:
-            #         if demand <440:
-            #             p = dict1[year]
-            #             b.SetField("price", p)
-            #         elif 440 <= demand < 880:
-            #             p = dict2[year]
-            #             b.SetField("price", p)
-            #         elif demand >= 880:
-            #             p = dict3[year]
-            #             b.SetField("price", p)
-            # self.parcel.finalise()
